38e51f40d93da18f62103d34e8bc55ea878b682e
Inchworm333 <Inchworm333@gmail.com>
2018-11-06 14:27:58 -0500

    Fix

--- Add_to_chrome.cmd
+++ Add_to_chrome.cmd
@@ -1,2 +1,2 @@
 cd "C:\Users\Inchw\AppData\Local\Google\Chrome SxS\Application"
-chrome.exe --load-extension=C:\Users\Inchw\Desktop\Better_MyCourses\Better_MyCourses mycourses.rit.edu/d2l/home
+chrome.exe --load-extension=C:\Users\Inchw\Desktop\Better-MyCourses\Better-MyCourses mycourses.rit.edu/d2l/home
