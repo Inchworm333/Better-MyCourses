491ad3d57d2c78e8a1f74ce675abad6a22cb12a1
Inchworm333 <inchworm333@gmail.com>
2019-01-30 15:10:47 -0500

    Updated EVERYTHING!!!

---
+++ Better-MyCourses/js/custom-course-image.js
@@ -0,0 +1,2 @@
+var changeImageButton = document.getElementsByTagName("d2l-dropdown-menu").getElementsByTagName("d2l-menu-item")[0];
+changeImageButton.addEventListener("click", function(){alert("itworked!")});
