0aebccb741fcaf68b5dc4fed55a1d91bbf7d8623
Inchworm333 <inchworm333@gmail.com>
2019-02-13 20:46:33 -0500

    Updated options so clicking the checkbox actually brings up the prompt for the optional permissions. Still need to fix removing them.

--- Better-MyCourses/js/options.js
+++ Better-MyCourses/js/options.js
@@ -4,10 +4,8 @@
 
 function save_options() {
 	let signin_skip = document.getElementById('signin_skip');
-	let duo_skip = document.getElementById('duo_skip');
 	chrome.storage.sync.set({
-		signin_skip: signin_skip,
-		duo_skip: duo_skip
+		signin_skip: signin_skip
 	}, function () {
 		var status = document.getElementById('status');
 		status.style.color = 'green';
@@ -26,23 +24,22 @@ function restore_options() {
 	// Use default value color = 'red' and likesColor = true.
 	chrome.storage.sync.get({
 		signin_skip: false,
-		duo_skip: false
 	}, function (items) {
 		(document.getElementById('signin_skip').innerHTML).checked = items.signin_skip;
-		(document.getElementById('duo_skip').innerHTML).checked = items.duo_skip;
 	});
 }
 
 
 function request_optional(option, permissions) {
-	chrome.permissions.contains({permissions: ['cookies']}, function(result){
-		if (!result) {
-			return permissions.permissions = permissions.permissions + 'cookies';
-		}
-	});
+	// chrome.permissions.contains({permissions: ['cookies']}, function(result){
+	// 	if (!result) {
+	// 		permissions.permissions.push('cookies');
+	// 	}
+	// });
+	console.log(permissions);
 	chrome.permissions.request(permissions,
 		function (granted) {
-			var status = document.getElementById('status');
+			let status = document.getElementById('status');
 			if (granted) {
 				option.checked = true;
 				//todo run script for option
@@ -61,13 +58,13 @@ function request_optional(option, permissions) {
 }
 
 
-function remove_optional(option, permissions) {
-	chrome.permissions.contains({origins: ['https://shibboleth.main.ad.rit.edu/*',
-			'https://*.duosecurity.com/*']}, function(result) {
-		if(!result) { permissions.permissions = permissions.permissions + 'cookies';}
-		});
+function remove_optional(permissions) {
+	// chrome.permissions.contains({origins: ['https://shibboleth.main.ad.rit.edu/*']},
+	// 	function(result) {
+	// 	if(!result) { permissions.permissions.push('cookies');}
+	// 	});
 	chrome.permissions.remove(permissions, function (removed) {
-		var status = document.getElementById('status');
+		let status = document.getElementById('status');
 		if (removed) {
 			status.style.color = 'green';
 			status.textContent = 'Optional permissions removed successfully.';
@@ -86,15 +83,13 @@ function remove_optional(option, permissions) {
 function permission_selector(option) {
 	var origin;
 	if(option === 'signin_skip') {
-		origin = {origins: ['https://shibboleth.main.ad.rit.edu/*']}
-	} else {
-		origin = {origins: ['https://*.duosecurity.com/*']}
+		origin = {origins: ['https://shibboleth.main.ad.rit.edu/*'], permissions: ['cookies']}
 	}
 	option = document.getElementById(option);
-	
-	if(option.checked === true) { //disabled, need to enable
+
+	if (option.checked === true) { //Checkbox has just been checked!!! Need to enable.
 		request_optional(option, origin)
-	} else { //enabled, need to disable
+	} else { //Checkbox has just been unchecked!!! Need to disable.
 		remove_optional(option, origin)
 	}
 }
@@ -104,8 +99,8 @@ document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('signin_skip').addEventListener('click', function () {
 	permission_selector('signin_skip'); });
-document.getElementById('duo_skip').addEventListener('click',function () {
-	permission_selector('duo_skip'); });
-document.addEventListener(chrome.permissions.onAdded.addListener(function (permissions) {
-	if (permissions.origins.includes("domain:"))
-}))
+document.getElementById('options').addEventListener('click', function () {
+	chrome.permissions.getAll(function (permissions) {
+		console.log(permissions);
+	})
+});
