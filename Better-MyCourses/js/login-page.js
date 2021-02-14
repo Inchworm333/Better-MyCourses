f19ab33174cb8dd9a226fe772e1ba2eb75dc40de
Inchworm333 <nxv7610@g.rit.edu>
2021-02-14 17:45:34 -0500

    Updated login page theme. Fixed some text boxes not being colored properly. Somehow broke the auto two factor authentication again.

--- Better-MyCourses/js/login-page.js
+++ Better-MyCourses/js/login-page.js
@@ -1,12 +1,5 @@
-document.querySelector('body > div > div:nth-child(3)').remove();
-
-document.querySelectorAll('.col-md-3 .support img').forEach((img) => {
-	let src = img.src.replace('https://mycourses.rit.edu/d2l/loginh/img/', '').split('.')[0];
-
-	img.src = chrome.runtime.getURL(`img/login-icons/${src}.svg`);
-	img.width = '72';
-	img.height = '72';
+window.addEventListener('load', () => {
+	let image = document.querySelector('img:nth-of-type(1)');
+	image.load = 'eager';
+	image.src = chrome.runtime.getURL('img/login-icons/Login-logo.svg');
 })
-
-document.querySelector('body > br').nextSibling.remove();
-document.querySelector('body > br').remove();
