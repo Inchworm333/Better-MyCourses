10d47b334220a11fba3433376d418c73f28066e7
Inchworm333 <nxv7610@g.rit.edu>
2021-06-25 19:37:43 -0400

    Fixed user uploaded html having custom styling (removes all custom user styling).

--- Better-MyCourses/js/css-change.js
+++ Better-MyCourses/js/css-change.js
@@ -5,3 +5,31 @@
 //let homeLink = document.querySelector('d2l-navigation-link-image');
 
 //homeLink.style.setProperty('filter', 'invert(1)');
+
+// https://github.com/Qix-/color
+// https://browserify.org/
+
+window.onload = () => {
+	let link = document.createElement("link");
+	link.href = chrome.runtime.getURL('css/frame.css')
+	link.rel = "stylesheet";
+	link.type = "text/css";
+
+	document.querySelectorAll('.d2l-iframe-fit-user-content').forEach(frame => {
+		frame.contentDocument.head.appendChild(link);
+	})
+
+	document.querySelectorAll('.d2l-iframe-fit-user-content').forEach(frame => {
+		frame.contentDocument.querySelectorAll('[style]').forEach(element => {
+			if (element.style.backgroundColor === 'white') {
+				element.style.backgroundColor = 'transparent';
+			} else {
+				element.style.backgroundColor = 'hue-rotate:180deg'
+			}
+		})
+	})
+
+	document.querySelectorAll('.d2l-htmlblock-untrusted [style]').forEach(element => {
+		element.style = '';
+	})
+}
